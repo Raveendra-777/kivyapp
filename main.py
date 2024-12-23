e20bf4128c83fc6c680382e66d675d1220d4ed6f
Raveendra-777 <raveendravarmapy@gmail.com>
2024-12-23 13:11:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -363,7 +363,7 @@ ScreenManager:
             
 """
 import base64
-token = "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"
+token = "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"
 repo_name = "Raveendra-777/new"
 def text_to_binary(text):
     binary_result = ''.join(format(ord(char), '08b') for char in text)
